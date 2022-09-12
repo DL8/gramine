af4a42d84781693cdcf6d98be775fe3c57dde966
Lavi, Nir <nir.lavi@intel.com>
2022-09-12 10:01:25 +0300

    fixup! [python,ci-examples] Support SGX extended product ID

--- CI-Examples/kss-helloworld/kss-helloworld.c
+++ CI-Examples/kss-helloworld/kss-helloworld.c
@@ -5,10 +5,10 @@
 
 #define MAX_DIGITS (256ULL) // Should be more than enough to contain all buffers
 
-static void blob2hex(const void *input, size_t len, char *output) {
+static void blob2hex(const void* input, size_t len, char* output) {
     static const char digits[] = "0123456789abcdef";
     for(size_t i=0;i<len;i++) {
-        uint8_t byte = *((uint8_t *)input + i);
+        uint8_t byte = *((uint8_t *) input + i);
         output[2*i] = digits[byte >> 4];
         output[2*i+1] = digits[byte & 0xf];
     }
@@ -17,22 +17,22 @@ static void blob2hex(const void *input, size_t len, char *output) {
 
 int main(void) {
     char print_buffer[MAX_DIGITS];
-    FILE *attestation_type_fd = NULL;
-    FILE *report_fd = NULL;
+    FILE* attestation_type_fd = NULL;
+    FILE* report_fd = NULL;
     sgx_report_t report;
 
     attestation_type_fd = fopen("/dev/attestation/attestation_type", "rb");
     if(attestation_type_fd == NULL) {
-	    fprintf(stderr, "Failed to open attestation type handle\n");
-	    return 1;
+        fprintf(stderr, "Failed to open attestation type handle\n");
+        return 1;
     }
     if(fread(print_buffer, 1, sizeof(print_buffer), attestation_type_fd) < 0) {
-	    fprintf(stderr, "Failed to read attestation type\n");
-	    return 2;
+        fprintf(stderr, "Failed to read attestation type\n");
+        return 2;
     }
     if(strcmp(print_buffer, "none") == 0) {
-	    fprintf(stderr, "Must be built with remote attestation\n");
-	    return 3;
+        fprintf(stderr, "Must be built with remote attestation\n");
+        return 3;
     }
     fclose(attestation_type_fd);
 
@@ -47,7 +47,7 @@ int main(void) {
         return 5;
     }
     fclose(report_fd);
-    printf("Hello, world\n");
+
     printf("isvprodid = %04x\n", report.body.isv_prod_id);
     printf("isvsvn = %d\n", report.body.isv_svn);
     blob2hex(report.body.isv_ext_prod_id, sizeof(report.body.isv_ext_prod_id), print_buffer);
